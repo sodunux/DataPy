f07ed143c0fca8e65fb61da3bcb420d7e64dc13f
Sodunux <sodunux@163.com>
2018-09-22 11:01:59 +0800

    增加数据分析

--- DataPy.py
+++ DataPy.py
@@ -150,6 +150,7 @@ class DataNumpy:
 class DataPandas:
     def __init__(self):
         plt.rcParams['font.sans-serif'] = ['SimHei']  # 中文字体设置
+
         # plt.rcParams['font.family'] = ['STFangsong']
         # self.font = FontProperties(fname='/Library/Fonts/Songti.ttc')
 
@@ -173,16 +174,15 @@ class DataPandas:
         print(obj.index)
         print(obj['d'])
 
-    def Demo3(self):
-        filename = open('601069.csv', encoding='gbk')
+    def GetDescribe(self, filenamestr):
+        filename = open(filenamestr, encoding='gbk')
         df = pd.read_csv(filename)
         df = df[df['收盘价'] > 0]
         del df['股票代码']
         del df['名称']
         del df['日期']
         df.index = range(len(df), 0, -1)
-        #del df['Unnamed: 0']
-        print(df)
+        # del df['Unnamed: 0']
         df.to_csv('remove_zero.csv')
         kpj = df['开盘价']
         spj = df['收盘价']
@@ -195,23 +195,100 @@ class DataPandas:
         cjje = df['成交金额']
         zsz = df['总市值']
         ltsz = df['流通市值']
+        zde = np.asarray(zde, dtype=np.float64)
+        zdf = np.asarray(zdf, dtype=np.float64)
+        zde = Series(zde, index=range(len(zde), 0, -1))
+        zdf = Series(zdf, index=range(len(zdf), 0, -1))
+        print("***************" + filenamestr + "***************")
+        print(spj.describe())
+
+    def Hsl_vs_Spj(self, filenamestr):
+        filename = open(filenamestr, encoding='gbk')
+        df = pd.read_csv(filename)
+        df = df[df['收盘价'] > 0]
+        del df['股票代码']
+        del df['名称']
+        del df['日期']
+        df.index = range(len(df), 0, -1)
+        # del df['Unnamed: 0']
+        df.to_csv('remove_zero.csv')
+        kpj = df['开盘价']
+        spj = df['收盘价']
+        zgj = df['最高价']
+        zdj = df['最低价']
+        zde = df['涨跌额']
+        zdf = df['涨跌幅']
+        hsl = df['换手率']
+        cjl = df['成交量']
+        cjje = df['成交金额']
+        zsz = df['总市值']
+        ltsz = df['流通市值']
+        zde = np.asarray(zde, dtype=np.float64)
+        zdf = np.asarray(zdf, dtype=np.float64)
+        zde = Series(zde, index=range(len(zde), 0, -1))
+        zdf = Series(zdf, index=range(len(zdf), 0, -1))
+        plt.plot(spj.index, spj.values * 10, label="收盘价")
+        plt.plot(hsl.index, hsl.values * 6, label="换手率")
+        plt.legend()
+        plt.grid()
+        plt.title(filenamestr)
+        plt.show()
+
+    def Demo3(self):
+        filename = open('601989.csv', encoding='gbk')
+        df = pd.read_csv(filename)
+        df = df[df['收盘价'] > 0]
+        del df['股票代码']
+        del df['名称']
+        del df['日期']
+        df.index = range(len(df), 0, -1)
+        # del df['Unnamed: 0']
+        df.to_csv('remove_zero.csv')
+        kpj = df['开盘价']
+        spj = df['收盘价']
+        zgj = df['最高价']
+        zdj = df['最低价']
+        zde = df['涨跌额']
+        zdf = df['涨跌幅']
+        hsl = df['换手率']
+        cjl = df['成交量']
+        cjje = df['成交金额']
+        zsz = df['总市值']
+        ltsz = df['流通市值']
+        zde = np.asarray(zde, dtype=np.float64)
+        zdf = np.asarray(zdf, dtype=np.float64)
+        zde = Series(zde, index=range(len(zde), 0, -1))
+        zdf = Series(zdf, index=range(len(zdf), 0, -1))
         # zsz.plot()
         # ltsz.plot()
         # print(hsl)
-        #plt.subplot(2, 2, 1)
-        #plt.plot(hsl.index, hsl.values)
-        #plt.subplot(2, 2, 2)
-        #plt.plot(cjl.index, cjl.values)
-        #plt.subplot(2, 2, 3)
-        plt.plot(spj.index, spj.values * 10)
-        plt.plot(hsl.index, hsl.values * 6)
-        plt.grid()
-        # zdf.plot()r
+        # plt.subplot(2, 2, 1)
+        # plt.plot(hsl.index, hsl.values)
+        # plt.subplot(2, 2, 2)
+        # plt.plot(cjl.index, cjl.values)
+        # plt.subplot(2, 2, 3)
+        # plt.plot(spj.index, spj.values * 10, label="收盘价")
+        # plt.plot(hsl.index, hsl.values * 6, label="换手率")
+
+        # plt.scatter(zde.index, zde.values + 10, label='涨跌额')
+        # plt.plot(zde.index, zde.values * 10 + 20, label="涨跌额")
+        # plt.plot(zdf.index, zdf.values / 10, label="涨跌幅")
+        # plt.plot(cjje.index, cjje.values / 100000000, label="成交金额")
+        # plt.plot(cjje.index, cjl.values * zgj.values /100000000, label="成交量x最高价")
+        # plt.plot(cjje.index, cjl.values * zdj.values /100000000, label="成交量x最低价")
+        # plt.plot(zde.index,zde.values*10,label="涨跌额")
+        # plt.plot(hsl.index,hsl.values,label="换手率")
+        # plt.plot(cjl.index, cjl.values / 500000, label="成交量")
+        # plt.plot(zsz.index,zsz.values,label="总市值")
+        # plt.plot(ltsz.index, ltsz.values / 50000000, label="流通市值")
+        # plt.grid()
+        # zdf.plot()
         # spj.plot()
         # zgj.plot()
         # zdj.plot()
         # plt.legend()
-        plt.show()
+        # plt.show()
+        spj.describe()
         # print(df.describe())
         # print(spj.describe())
         # print(spj.count())
@@ -235,11 +312,27 @@ class DataPandas:
         # plt.legend()
         # plt.show()
 
+    def Demo4(self):
+        self.GetDescribe('601069.csv')
+        self.GetDescribe('601989.csv')
+        self.GetDescribe('600460.csv')
+        self.Hsl_vs_Spj('601069.csv')
+        self.Hsl_vs_Spj('601989.csv')
+        # self.Hsl_vs_Spj('600460.csv')
+
+    def Demo5(self, step):
+        ix = np.arange(11)
+        iy = (1 + step)**ix
+        plt.plot(ix, iy)
+        # plt.legend()
+        plt.grid()
+        plt.show()
+
 
 if __name__ == '__main__':
-    #dp = DataPlot()
+    # dp = DataPlot()
     # dp.Demo()
     # dn=DataNumpy()
     # dn.Base()
     ddp = DataPandas()
-    ddp.Demo3()
+    ddp.Demo5(0.1)
--- PandasPy.py
+++ PandasPy.py
@@ -2,13 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.font_manager import FontProperties
-font = FontProperties(fname='/Library/Fonts/Songti.ttc')
 df = pd.read_csv('remove_zero.csv')
 del df['Unnamed: 0']
 df.to_csv('remove_zero.csv')
 zgj = df['最高价']
 
-#zgj.plot()
-plt.plot(zgj.index, zgj.values, fontproperties=font)
+# zgj.plot()
+plt.plot(zgj.index, zgj.values,label="最高价")
 plt.legend()
 plt.show()
