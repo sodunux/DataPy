b0a1b6bdffcc80faf16b948ef9fb430c86472bf6
Sodunux <sodunux@163.com>
2018-09-24 12:49:55 +0800

    增加部分tushare数据

--- PandasPy.py
+++ PandasPy.py
@@ -1,13 +1,3 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from matplotlib.font_manager import FontProperties
-df = pd.read_csv('remove_zero.csv')
-del df['Unnamed: 0']
-df.to_csv('remove_zero.csv')
-zgj = df['最高价']
-
-# zgj.plot()
-plt.plot(zgj.index, zgj.values,label="最高价")
-plt.legend()
-plt.show()
--- m2_pro.py
+++ m2_pro.py
@@ -6,10 +6,10 @@ import tushare as ts
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 中文字体设置
 filename = open('货币供应量.csv', encoding='gbk')
 dt = pd.read_csv(filename)
-dt = dt['m2']
-dt = dt[0:273]
-dt.index = np.arange(273, 0, -1)
-dt=dt.astype('float64')
+dt = dt[['m2', 'm1', 'm0', 'sd', 'qm', 'cd']]
+dt = dt[0:170]
+dt.index = np.arange(170, 0, -1)
+dt = dt.astype('float64')
 dt.plot()
-#plt.plot(dt.index,dt.values)
+# plt.plot(dt.index,dt.values)
 plt.show()
